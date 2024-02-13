6b1146d3b3e9cf7246fc94bf5e630bb6e7b88425
DESKTOP-MLLLST5\Julian <kidsfreej@gmail.com>
2024-02-13 16:33:17 -0500

    deleted useless folder

--- test_files/unit1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int thesymbol = 100;
-extern void print2();
-int main(){
-    printf("%d\n",thesymbol);
-    print2();
-    return 0;
-}
--- test_files/unit2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int thesymbol = 500;
-
-void print2(){
-    printf("%d\n",thesymbol);
-}
